0533deccb43e1e29457bd1ac6b17833a5166b5fe
avinocur <avinocur@despegar.com>
2015-04-19 14:19:30 -0300

    Arreglo RR. Hay logs comentados a cerr que habria que borrar. Los dejo por ahora

--- tp1/tp1-alumnos/sched_rr.cpp
+++ tp1/tp1-alumnos/sched_rr.cpp
@@ -9,16 +9,20 @@ using namespace std;
 SchedRR::SchedRR(vector<int> argn) {
 	// Round robin recibe la cantidad de cores y sus cpu_quantum por parámetro
 	cores = argn[0];
+	//cerr << "Cores: " << cores << endl;
+	cpu_quantum.resize(cores);
 	for(int i=1; i < (int)argn.size(); i++){
-		cpu_quantum[i] = argn[i];
+		//cerr << "Quantum: " << argn[i] << endl; 
+		cpu_quantum[i-1] = argn[i];
 	}
 }
 
 SchedRR::~SchedRR() {
+	
 }
 
-void initialize() {
-	for(int core=1; core<=cores; core++){
+void SchedRR::initialize() {
+	for(int core=0; core<cores; core++){
 		remaining_q_per_core[core] = TaskQuantumPair(IDLE_TASK, 0);
 	} 
 }
@@ -34,34 +38,44 @@ void SchedRR::unblock(int pid) {
 
 int SchedRR::tick(int cpu, const enum Motivo m) {
 	int sig;
+	int core = cpu;
 	if(m == EXIT){
 		sig = next();
-		loadOnCore(sig, cpu);
+		loadOnCore(sig, core);
+		//cerr << "EXIT. El proximo es: " << sig << endl;
 	}
 	else if(m == TICK){
-		TaskQuantumPair runningTask = remaining_q_per_core[cpu];
+		TaskQuantumPair runningTask = remaining_q_per_core[core];
+		//cerr << "TICK. Esta corriendo: " << runningTask.first << ". Le quedan: " << runningTask.second << endl;
 		// Si no hay mas quantum, pasar a la siguiente tarea
-		if(runningTask.second <= 0){
+		if(runningTask.second <= 1){
 			sig = next();
-			loadOnCore(sig, cpu);
+			loadOnCore(sig, core);
 			// la tarea actual pasa a Ready: al final de la cola.
-			process.push(runningTask.first);
+			if(runningTask.first != IDLE_TASK){
+				process.push(runningTask.first);
+			}
+			//cerr << "El proximo es: " << sig << endl;
 		}
 		else {
 			// La tarea sigue corriendo
 			runningTask.second -= 1;
+			remaining_q_per_core[core] = runningTask;
 			sig = runningTask.first;
+			//cerr << "Sigue corriendo: " << sig << endl;
 		}
 	}
 	else {  // BLOQ
 		sig = next();
-		loadOnCore(sig, cpu);
+		loadOnCore(sig, core);
+		
+		//cerr << "BLOQ. El proximo es: " << sig << endl;
 	}
 	
 	return sig;
 }
 
-void loadOnCore(int pid, int core){
+void SchedRR::loadOnCore(int pid, int core){
 	remaining_q_per_core[core].first = pid;
 	
 	// resolver el quantum de la tarea (0 si es IDLE)
@@ -77,5 +91,7 @@ int SchedRR::next() {
 	if(process.empty()){
 		return IDLE_TASK;
 	}
-	return process.pop();
+	int next = process.front();
+	process.pop();
+	return next;
 }
