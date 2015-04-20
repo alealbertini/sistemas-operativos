69922467481bab09ac69d29976fedc2c0bfd747d
U-RECONQUISTA\XA03955 <XA03955@pc0045800.bf.arg.igrupobbva>
2015-04-20 19:21:16 -0300

    Corregi el TaskBatch, ahora funciona como piden en el enunciado.

--- tp1/tp1-alumnos/tasks.cpp
+++ tp1/tp1-alumnos/tasks.cpp
@@ -33,6 +33,60 @@ void TaskConBloqueo(int pid, vector<int> params) {
 	}
 }
 
+
+// params: total_cpu, cant_bloqueos
+void TaskBatch(int pid, vector<int> params){
+        int total_cpu = params[0];
+        int cant_bloqueos = params[1];
+
+    //Inicializamos la semilla de la secuencia de numeros aleatorios.
+        srand(time(NULL));
+
+        //En este vector vamos a guardar los ticks en los que hay llamadas bloqueantes
+        //ticks_bloq[i] == 1 => en el tick i se usa CPU y hay una llamada bloqueante.
+        //ticks_bloq[i] == 0 => en el tick i solo se usa CPU
+    vector<int> ticks_bloq;
+        ticks_bloq.resize(total_cpu, 0);
+
+        int ticks_disponibles = total_cpu;
+        while(cant_bloqueos>0){
+            //Sorteamos el tick bloqueante.
+                int tick_elegido = (rand() % ticks_disponibles);
+
+                //Nos posicionamos en el tick_elegido
+                //buscamos el iesimo 0 salteando los 1
+                int contador=0;
+                for(int i=0; contador!=tick_elegido; i++)
+                        if(ticks_bloq[i]==0)
+                                        contador++;
+
+                //Seteamos la llamada bloqueante.
+                ticks_bloq[contador] = 1;
+
+                //actualizamos, queda un tick menos y una llamada bloqueante menos.
+                cant_bloqueos--;
+                ticks_disponibles--;
+        }
+
+
+        //Juntamos todos el uso de CPU consecutivo, si encontramos una llamada bloqueante "flusheamos" el uso de CPU.
+        int cpu_acumulado = 0;
+        for(int i=0; i < total_cpu; i++){
+                cpu_acumulado++;
+
+                if(ticks_bloq[i]==1){
+                        uso_CPU(pid, cpu_acumulado);
+                        uso_IO(pid, 1);
+                        cpu_acumulado = 0;
+                }
+        }
+        //Si no hubo llamada bloqueante en el ultimo tick, hay que informar el uso de CPU restante.
+        if(cpu_acumulado>0){
+                uso_CPU(pid, cpu_acumulado);
+        }
+
+}
+/*
 void TaskBatch(int pid, vector<int> params){	// params: total_cpu, cant_bloqueos
 	int total_cpu = params[0];
 	int cant_bloqueos = params[1];
@@ -55,6 +109,7 @@ void TaskBatch(int pid, vector<int> params){	// params: total_cpu, cant_bloqueos
 		// total_cpu es la cantidad de ticks que me quedan disponibles para usar.
 	}
 }
+*/
 
 void TaskConsola(int pid, vector<int> params) {  // params: n, bmin, bmax
 	int n = params[0];
