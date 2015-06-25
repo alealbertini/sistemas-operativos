015f785af68a30e4eff647eb52b9b5e403b3dd9a
brumraul <brumraul@gmail.com>
2015-06-25 15:20:39 -0300

    El script esta terminado solo falta cambiarle la configuracion para que ejecute de una manera o de otra.

--- tp1/tp1-alumnos/comando.sh
+++ tp1/tp1-alumnos/comando.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+CONFIG_CORES="1 2 4 6 8"
+CONFIG_QUANTUM=$(seq 1 2 10)
+CONFIG_VECES=20
 if [ -z "$1" ];
 then
 	echo "Uso: comando.sh <archivo.tsk>"
@@ -12,8 +15,8 @@ then
 fi
 FILE_SALIDA=salida_$(basename ${FILE})_.csv
 rm -f ${FILE_SALIDA}
-for core in $(echo 1 2 4 6 8); do 
-for quant in $(seq 1 2 10); do
-echo "Corrida para ${FILE} con ${core} cores y ${quant} como quantum...";
-for i in $(seq 1 10); do
+for core in $(echo ${CONFIG_QUANTUM}); do 
+for quant in $(echo ${CONFIG_QUANTUM}); do
+echo "Corrida para ${FILE} con ${core} cores y ${quant} como quantum iterando ${CONFIG_VECES} veces...";
+for i in $(seq 1 ${CONFIG_VECES}); do
 ./simusched ejercicio7.tsk ${core} 1 2 SchedRR ${quant} 2>/dev/null | awk -f filtro.awk; done | awk  -v ncore=${core} -v quantum=${quant} -v print_varianza="off"  -f filtro2.awk >> ${FILE_SALIDA}; done; done
